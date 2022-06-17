0b9c3b1f18e95be0069ebd46f3c075f2a6413bb3
zbl14 <ifthereisoneday@gmail.com>
2022-06-16 21:32:41 -0700

    creat a construtor called Player, return name and an array named board, test passed

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 ```sh
-Describe Game()
+Describe Player()
 ```
-Test: "It should return an object with name and board object"\
-Code: let game = new Game("Ben");\
-Expected Output: Game {name: 'Ben', board: {…}}
+Test: "It should return an object with name and board array"\
+Code: let player1 = new Game("Ben");\
+Expected Output: 
+Player {name: 'Ben', board: Array(9)}
+board: (9) ['x', 'x', 'x', '3', '4', '5', '6', '7', '8']
+name: "Ben"
 
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,45 @@
-function Game (name) {
+function Player(name) {
   this.name = name;
-  this.board = {};
+  this.board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
 }
 
+let player1 = new Player("Ben");
+let player2 = new Player("Carrie");
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Board() {
+//   this.space = {};
+//   this.currentId = 0 ;
+// }
+
+// Board.prototype.addSpace = function(space) {
+//   contact.id = this.assignId();
+//   this.spaces[position.id] = space;
+// };
+
+// Board.prototype.assignedId = function() {
+//   this.currentId += 1;
+//   return this.currentId;
+// }
+
+// Board.prototype.findSpace = function(id) {
+//   if (this.spaces[id] != undefined) {
+//     return this.spaces[id];
+//   }
+//   return false
+// }
+
+// function Space(x, y) {
+//   this.position = (x,y)
+// }
